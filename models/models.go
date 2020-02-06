573ab3a7396a080c18c14438ac8c45e305962515
Muguku <hello@muguku.co.ke>
2020-02-06 19:59:50 +0300

    do not validate ID since not in input, add UserID to claims

--- models/models.go
+++ models/models.go
@@ -8,7 +8,7 @@ import (
 
 // GormUser : for postgres
 type GormUser struct {
-	ID          string `gorm:"column:id; PRIMARY_KEY" json:"id" validate:"required"`
+	ID          string `gorm:"column:id; PRIMARY_KEY" json:"id"`
 	FirstName   string `json:"firstName" validate:"required"`
 	LastName    string `json:"lastName" validate:"required"`
 	Email       string `gorm:"type:varchar(100);unique_index" json:"email" validate:"required,email"`
@@ -22,6 +22,7 @@ type GormUser struct {
 //Claims : struct that will be encoded to a JWT.
 // We add jwt.StandardClaims as an embedded type, to provide fields like expiry time
 type Claims struct {
+	UserID   string `json:"userid"`
 	Username string `json:"username"`
 	jwt.StandardClaims
 }
