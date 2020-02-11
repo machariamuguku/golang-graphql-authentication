380dcc511e657cbb4c61c3c99c63eb10eb36c669
Muguku <hello@muguku.co.ke>
2020-02-11 03:43:57 +0300

    added auto migrate models

--- migrations/migrations.go
+++ migrations/migrations.go
@@ -11,6 +11,15 @@ func main() {
 		panic(err)
 	}
 
+	// automigrate doesn't clear existing data
+	// only adds none existent columns
+
+	// automigrate one
+	// db.AutoMigrate(models.GormUser{})
+
+	// automigrate many
+	// db.AutoMigrate(&GormUser{}, &GormProduct{}, &GormOrder{})
+
 	db.DropTableIfExists(&models.GormUser{})
 	db.CreateTable(&models.GormUser{})
 }
